6e913e177db5c27afac7116da9756a1e9bb1ac9b
Secure <secure@error.error.org>
2025-01-07 01:13:56 -0600

    variable and data types done

---
+++ variable&data_types/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* var x int = 20
+	var s string = "hello golang"
+	var f float32 = 12.05
+	var b bool = false */
+	x := 20
+	s := "hello go"
+	f := 12.05
+	b := false
+	fmt.Println(x)
+	fmt.Println(s);
+	fmt.Println(f);
+	fmt.Println(b);
+}
